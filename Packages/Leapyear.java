754dd1ba9cbcb895cf65029cbf9e8f9ec4ff1487
gayatrriiii <bhosalegayatri30@gmail.com>
2021-08-19 21:26:46 +0530

    files modified

---
+++ Packages/Leapyear.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Leapyear {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter a year: ");
+        int year;
+        year = input.nextInt();
+
+        if ((year % 4 == 0) && (year % 100 != 0)
+                || ((year % 400 == 0))) {
+            System.out.println(year + " is a Leap year.");
+        }
+        else System.out.println(year + " is not a Leap year.");
+    }
+}
