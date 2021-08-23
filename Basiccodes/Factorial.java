85b40ccd277e9b29032482b320443820aff59143
gayatrriiii <bhosalegayatri30@gmail.com>
2021-08-24 00:45:07 +0530

    files added

---
+++ Basiccodes/Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n;
+        n = input.nextInt();
+        int factor = 1;
+
+        for(int count = 1; count <= n; count++){
+            factor *= count;
+        }
+        System.out.print("Factorial of number: "+ factor);
+    }
+}
