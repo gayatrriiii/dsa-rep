a33c72499f9699f6bd5c0652fc549f3d76a2ca51
gayatrriiii <bhosalegayatri30@gmail.com>
2021-08-29 22:58:47 +0530

    files added

--- Conditionals/AreaofCircle.java
+++ Conditionals/AreaofCircle.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class AreaofCircle {
-    public static <String> void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Please enter the radius of the circle: ");
+        float r = in.nextFloat();
+        float pi = 3.14f;
+        float area;
+
+        area = pi * (r * r);
+        System.out.println("The area of circle is: "+ area);
 
     }
 }
